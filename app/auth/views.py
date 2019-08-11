448428e0e59b2d894840593593d7962a95dcadee
Johnonyango <j.yayah7@gmail.com>
2019-08-11 14:24:36 +0300

    modified app views.py

--- app/auth/views.py
+++ app/auth/views.py
@@ -9,6 +9,21 @@ from ..email import mail_message
 
 
 
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+
+        mail_message("Welcome to Blog","email/welcome_user",user.email,user=user)
+
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/register.html',registration_form = form)    
+
+
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
@@ -36,16 +51,3 @@ def logout():
     return redirect(url_for("main.index"))    
 
 
-@auth.route('/register',methods = ["GET","POST"])
-def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
-        db.session.add(user)
-        db.session.commit()
-
-        mail_message("WELCOME TO BLOG POST","email/welcome_user",user.email,user=user)
-
-        return redirect(url_for('auth.login'))
-        title = "New Account"
-    return render_template('auth/register.html',registration_form = form)    
