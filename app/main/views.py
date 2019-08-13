80bc67fba8020fb43fd2418387c9a5464310b863
Johnonyango <j.yayah7@gmail.com>
2019-08-13 10:26:21 +0300

    consume API request

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,10 @@
 from flask import render_template,request,redirect,url_for,abort
-from . import main
-from .forms import PostForm, CommentForm, UpdateProfile, SubscribeForm
-from ..models import User, Post, Comment, Subscriber
 from flask_login import login_required, current_user
+from . import main
 from .. import db, photos
+from ..request import get_quote
+from ..models import User, Post, Comment, Subscriber
+from .forms import PostForm, CommentForm, UpdateProfile, SubscribeForm
 from .. email import mail_message
 
 
@@ -12,8 +13,9 @@ def index():
   
   title = 'Home - Welcome to Jiggy-Blogs'
   posts = Post.get_posts()
+  quote = get_quote()
 
-  return render_template('index.html', title = title, posts = posts)
+  return render_template('index.html', title = title, posts = posts, quote=quote)
 
 
 @main.route('/post/new', methods = ['GET','POST']) 
--- app/request.py
+++ app/request.py
@@ -4,9 +4,9 @@ from .models import Quote
 def get_quote():
 
     with urllib.request.urlopen('http://quotes.stormconsultancy.co.uk/random.json') as url:
-    quote_details_data = url.read()
-    quote_details_response = json.loads(quote_details_data)
-    quote_object = None
+        quote_details_data = url.read()
+        quote_details_response = json.loads(quote_details_data)
+        quote_object = None
     if quote_details_response:
         author = quote_details_response.get('author')
         quote = quote_details_response.get('quote')
